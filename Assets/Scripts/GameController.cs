28a48c742c9c85822151da7010ef089a10b4e70c
Paulo Victor <paulovictor.joao@gmail.com>
2017-04-03 20:09:21 -0300

    Adicionando os commits

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Estado estado { get; private set; }
+
+    public GameObject obstaculo;
+    public float espera;
+    public float tempoDestruicao;
+
+    public static GameController instancia = null;
+    private void Awake() {
+        if (instancia == null) {
+            instancia = this;
+        }
+        else if (instancia != null) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start () {
+        estado = Estado.AguardandoComecar;
+	}
+	
+
+	IEnumerator GerarObstaculos() {
+        while (GameController.instancia.estado == Estado.Jogando) {
+            Vector3 pos = new Vector3(2.1f, Random.Range(-3f, 2.1f), 0f);
+            GameObject obj = Instantiate(obstaculo, pos, Quaternion.identity) as GameObject;
+            Destroy(obj, tempoDestruicao);
+            yield return new WaitForSeconds(espera);
+        }
+    }
+
+    public void PlayerComecou() {
+        estado = Estado.Jogando;
+        StartCoroutine(GerarObstaculos());
+    }
+
+    public void PlayerMorreu() {
+        estado = Estado.GameOver;
+    } 
+		
+}
---
+++ Assets/Scripts/PranchaMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PranchaMove : MonoBehaviour {
+
+    public float velocidade;
+    public float limite;
+    public float retorno;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        Vector3 velocidadeVetorial = Vector3.left * velocidade;
+        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+
+        if(transform.position.x < limite) {
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
+        }
+	}
+}
