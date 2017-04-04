a74244adbec32040ac391def37b072ced71c4ff7
Paulo Victor <paulovictor.joao@gmail.com>
2017-04-03 22:21:53 -0300

    Ajustando as animações aos scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,57 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-    public float ForcaDoPulo = 10f;
-    public AudioClip somPulo;
-    public AudioClip somMorte;
-
-    private Animator anim;
-    private Rigidbody rb;
-    private AudioSource audioSource;
-    private bool pulando = false;
-
-	void Start () {
+ public class PlayerController : MonoBehaviour {
+ 
+     public float ForcaDoPulo = 10f;
+ 
+     private Animator anim;
+     private Rigidbody rb;
+     private bool pulando = false;
+ 
+ 	void Start() {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-	}
-
-	void Update () {
-        if (GameController.instancia.estado == Estado.Jogando || GameController.instancia.estado == Estado.AguardandoComecar) {
-            if (Input.GetMouseButtonDown(0)) {
-                anim.Play("pulando");
-                audioSource.PlayOneShot(somPulo);
-                rb.useGravity = true;
-                pulando = true;
-                if (GameController.instancia.estado == Estado.AguardandoComecar) {
-                    GameController.instancia.PlayerComecou();
-                }
-            }
-        }
-	}
-
+          }
+ 
+ 	void Update() {
+              if (Input.GetMouseButtonDown(0)) {
+            anim.Play("pulando");
+            rb.useGravity = true;
+            pulando = true;
+                    }
+          }
+ 
     void FixedUpdate() {
-        if (GameController.instancia.estado == Estado.Jogando) {
-            if (pulando) {
-                pulando = false;
-                rb.velocity = Vector3.zero;
-                rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse);
+                if (pulando) {
+            pulando = false;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse);
+                    }
             }
-        }
-    }
-
-    void OnCollisionEnter(Collision outro) {
-        if (GameController.instancia.estado == Estado.Jogando) {
-            if (outro.gameObject.tag == "obstaculo") {
-                rb.AddForce(new Vector3(-50f, 20f, 0f), ForceMode.Impulse);
-                rb.detectCollisions = false;
-                anim.Play("morrendo");
-                audioSource.PlayOneShot(somMorte);
-                GameController.instancia.PlayerMorreu();
-            }
-        }
-    }
-
-}
+ 
+ }
---
+++ Assets/Scripts/PlayerControllerFINAL.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerFINAL : MonoBehaviour {
+
+    public float ForcaDoPulo = 10f;
+    public AudioClip somPulo;
+    public AudioClip somMorte;
+
+    private Animator anim;
+    private Rigidbody rb;
+    private AudioSource audioSource;
+    private bool pulando = false;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        if (GameController.instancia.estado == Estado.Jogando || GameController.instancia.estado == Estado.AguardandoComecar) {
+            if (Input.GetMouseButtonDown(0)) {
+                anim.Play("pulando");
+                audioSource.PlayOneShot(somPulo);
+                rb.useGravity = true;
+                pulando = true;
+                if (GameController.instancia.estado == Estado.AguardandoComecar) {
+                    GameController.instancia.PlayerComecou();
+                }
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        if (GameController.instancia.estado == Estado.Jogando) {
+            if (pulando) {
+                pulando = false;
+                rb.velocity = Vector3.zero;
+                rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision outro) {
+        if (GameController.instancia.estado == Estado.Jogando) {
+            if (outro.gameObject.tag == "obstaculo") {
+                rb.AddForce(new Vector3(-50f, 20f, 0f), ForceMode.Impulse);
+                rb.detectCollisions = false;
+                anim.Play("morrendo");
+                audioSource.PlayOneShot(somMorte);
+                GameController.instancia.PlayerMorreu();
+            }
+        }
+    }
+
+}
